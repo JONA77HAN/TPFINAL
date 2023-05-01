c648d3ff3c8b4ade020dfda8b67a59232f53ad64
Jonathan <jonathancarranza70@gmail.com>
2023-04-30 21:18:32 -0300

    vamos q se puede

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ExperiencieComponent } from './components/experiencie/experiencie.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { HttpClientModule} from  '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    SkillsComponent,
+    ExperiencieComponent,
+    ProjectsComponent,
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
