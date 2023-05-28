2f0dd9fb54aa092399658c93db8a42fa7c8f49cc
Jonathan <jonathancarranza70@gmail.com>
2023-05-28 20:42:40 -0300

    agregamos data al json, y estudiamos lo de los constructores

--- src/app/components/experiencie/experiencie.component.ts
+++ src/app/components/experiencie/experiencie.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-experiencie',
   templateUrl: './experiencie.component.html',
   styleUrls: ['./experiencie.component.css']
 })
-export class ExperiencieComponent {
+export class ExperiencieComponent implements OnInit {
+ miPortfolio:any;
+  constructor (private datosPortfolio:PortfolioService) {}
 
+ngOnInit() : void {
+    this.datosPortfolio.obtenerDatos().subscribe(data =>{
+    console.log(data);
+    this.miPortfolio=data;
+  });
+}
 }
